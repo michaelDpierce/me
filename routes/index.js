062efca877eb64803bb47e53cad0072fb96f148b
Michael Pierce <michael@affektive.com>
2014-07-23 20:31:40 -0600

    Finishing up website

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'The index page!' })
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
 
-/* GET Hello World page. */
 router.get('/contact', function(req, res) {
     res.render('contact', { title: 'Michael Pierce' })
 });
 
-/* GET Hello World page. */
 router.get('/portfolio', function(req, res) {
     res.render('portfolio', { title: 'Michael Pierce' })
 });
+
